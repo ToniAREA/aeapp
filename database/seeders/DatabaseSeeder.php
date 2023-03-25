d3144df41edc2038a7d1fff6b6dfb86851f63eff
ToniAREA <43147720L@gmail.com>
2023-03-25 14:44:15 +0100

    updated DatabaseSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
             PermissionRoleTableSeeder::class,
             UsersTableSeeder::class,
             RoleUserTableSeeder::class,
+            EmployeesSeeder::class,
+            MarinasSeeder::class,
+            ClientsSeeder::class,
+            BoatsSeeder::class,
         ]);
     }
 }
