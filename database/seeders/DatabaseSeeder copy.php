5698a807f9cc28cf6c13a2a9815b9cb918aca4f1
ToniAREA <43147720L@gmail.com>
2023-03-25 14:51:41 +0100

    created 2 backup files to avoid wipping from QuickAdminPanel

---
+++ database/seeders/DatabaseSeeder copy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call([
+            PermissionsTableSeeder::class,
+            RolesTableSeeder::class,
+            PermissionRoleTableSeeder::class,
+            UsersTableSeeder::class,
+            RoleUserTableSeeder::class,
+            PriorityTableSeeder::class,
+            EmployeesSeeder::class,
+            MarinasSeeder::class,
+            ClientsSeeder::class,
+            BoatsSeeder::class,
+        ]);
+    }
+}
