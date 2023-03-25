f324e5d815903e04b52c457b675eb4a586b3f669
ToniAREA <43147720L@gmail.com>
2023-03-26 00:45:51 +0100

    push from vscode

--- database/seeders/MarinasSeeder.php
+++ database/seeders/MarinasSeeder.php
@@ -17,7 +17,7 @@ class MarinasSeeder extends Seeder
     {
         // Obtener datos de la tabla origen de la base de datos antigua
         $oldMarinas = DB::connection('mysql_old')->table('marinas')->get();
-
+        
         // Insertar los datos en la tabla destino de la nueva base de datos
         foreach ($oldMarinas as $marina) {
 
