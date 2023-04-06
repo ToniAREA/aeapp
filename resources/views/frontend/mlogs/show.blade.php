76a1c14038b8ca0a8db2d157ad285d2619844de4
ToniAREA <43147720L@gmail.com>
2023-04-06 20:44:00 +0200

    again

--- resources/views/frontend/mlogs/show.blade.php
+++ resources/views/frontend/mlogs/show.blade.php
@@ -6,13 +6,13 @@
 
             <div class="card">
                 <div class="card-header">
-                    {{ trans('global.show') }} {{ trans('cruds.mlog.title') }}
+                    {{ trans('global.show') }} {{ trans('cruds.mLog.title') }}
                 </div>
 
                 <div class="card-body">
                     <div class="form-group">
                         <div class="form-group">
-                            <a class="btn btn-default" href="{{ route('frontend.mlogs.index') }}">
+                            <a class="btn btn-default" href="{{ route('frontend.m-logs.index') }}">
                                 {{ trans('global.back_to_list') }}
                             </a>
                         </div>
@@ -20,82 +20,24 @@
                             <tbody>
                                 <tr>
                                     <th>
-                                        {{ trans('cruds.mlog.fields.id') }}
+                                        {{ trans('cruds.mLog.fields.id') }}
                                     </th>
                                     <td>
-                                        {{ $mlog->id }}
+                                        {{ $mLog->id }}
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>
-                                        {{ trans('cruds.mlog.fields.product') }}
+                                        {{ trans('cruds.mLog.fields.code') }}
                                     </th>
                                     <td>
-                                        {{ $mlog->product->name ?? '' }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.description') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->description }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.quantity') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->quantity }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.price_unit') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->price_unit }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.discount') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->discount }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.total') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->total }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.status') }}
-                                    </th>
-                                    <td>
-                                        {{ $mlog->status }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>
-                                        {{ trans('cruds.mlog.fields.tags') }}
-                                    </th>
-                                    <td>
-                                        @foreach($mlog->tags as $key => $tags)
-                                            <span class="label label-info">{{ $tags->name }}</span>
-                                        @endforeach
+                                        {{ $mLog->code }}
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
                         <div class="form-group">
-                            <a class="btn btn-default" href="{{ route('frontend.mlogs.index') }}">
+                            <a class="btn btn-default" href="{{ route('frontend.m-logs.index') }}">
                                 {{ trans('global.back_to_list') }}
                             </a>
                         </div>
