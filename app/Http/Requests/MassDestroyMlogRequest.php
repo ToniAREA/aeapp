aadb84784a610828222b506f11ef5d23ea724758
ToniAREA <43147720L@gmail.com>
2023-04-06 21:13:21 +0200

    up working

--- app/Http/Requests/MassDestroyMlogRequest.php
+++ app/Http/Requests/MassDestroyMlogRequest.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Requests;
 
-use App\Models\MLog;
+use App\Models\Mlog;
 use Gate;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 
-class MassDestroyMLogRequest extends FormRequest
+class MassDestroyMlogRequest extends FormRequest
 {
     public function authorize()
     {
-        abort_if(Gate::denies('m_log_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('mlog_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
@@ -20,7 +20,7 @@ class MassDestroyMLogRequest extends FormRequest
     {
         return [
             'ids'   => 'required|array',
-            'ids.*' => 'exists:m_logs,id',
+            'ids.*' => 'exists:mlogs,id',
         ];
     }
 }
