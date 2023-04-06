e3125a5b548d0b484bb5dc7293ff498d7a960cd8
ToniAREA <43147720L@gmail.com>
2023-04-06 18:21:45 +0200

    MLogs to Mlogs

--- resources/views/frontend/mlogs/edit.blade.php
+++ resources/views/frontend/mlogs/edit.blade.php
@@ -6,104 +6,22 @@
 
             <div class="card">
                 <div class="card-header">
-                    {{ trans('global.edit') }} {{ trans('cruds.mlog.title_singular') }}
+                    {{ trans('global.edit') }} {{ trans('cruds.mLog.title_singular') }}
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route("frontend.mlogs.update", [$mlog->id]) }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route("frontend.m-logs.update", [$mLog->id]) }}" enctype="multipart/form-data">
                         @method('PUT')
                         @csrf
                         <div class="form-group">
-                            <label for="product_id">{{ trans('cruds.mlog.fields.product') }}</label>
-                            <select class="form-control select2" name="product_id" id="product_id">
-                                @foreach($products as $id => $entry)
-                                    <option value="{{ $id }}" {{ (old('product_id') ? old('product_id') : $mlog->product->id ?? '') == $id ? 'selected' : '' }}>{{ $entry }}</option>
-                                @endforeach
-                            </select>
-                            @if($errors->has('product'))
+                            <label for="code">{{ trans('cruds.mLog.fields.code') }}</label>
+                            <input class="form-control" type="text" name="code" id="code" value="{{ old('code', $mLog->code) }}">
+                            @if($errors->has('code'))
                                 <div class="invalid-feedback">
-                                    {{ $errors->first('product') }}
+                                    {{ $errors->first('code') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.product_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="description">{{ trans('cruds.mlog.fields.description') }}</label>
-                            <input class="form-control" type="text" name="description" id="description" value="{{ old('description', $mlog->description) }}">
-                            @if($errors->has('description'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('description') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.description_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="quantity">{{ trans('cruds.mlog.fields.quantity') }}</label>
-                            <input class="form-control" type="number" name="quantity" id="quantity" value="{{ old('quantity', $mlog->quantity) }}" step="0.01">
-                            @if($errors->has('quantity'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('quantity') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.quantity_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="price_unit">{{ trans('cruds.mlog.fields.price_unit') }}</label>
-                            <input class="form-control" type="number" name="price_unit" id="price_unit" value="{{ old('price_unit', $mlog->price_unit) }}" step="0.01">
-                            @if($errors->has('price_unit'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('price_unit') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.price_unit_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="discount">{{ trans('cruds.mlog.fields.discount') }}</label>
-                            <input class="form-control" type="number" name="discount" id="discount" value="{{ old('discount', $mlog->discount) }}" step="0.01">
-                            @if($errors->has('discount'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('discount') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.discount_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="total">{{ trans('cruds.mlog.fields.total') }}</label>
-                            <input class="form-control" type="number" name="total" id="total" value="{{ old('total', $mlog->total) }}" step="0.01">
-                            @if($errors->has('total'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('total') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.total_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="status">{{ trans('cruds.mlog.fields.status') }}</label>
-                            <input class="form-control" type="text" name="status" id="status" value="{{ old('status', $mlog->status) }}">
-                            @if($errors->has('status'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('status') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.status_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <label for="tags">{{ trans('cruds.mlog.fields.tags') }}</label>
-                            <div style="padding-bottom: 4px">
-                                <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                                <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
-                            </div>
-                            <select class="form-control select2" name="tags[]" id="tags" multiple>
-                                @foreach($tags as $id => $tag)
-                                    <option value="{{ $id }}" {{ (in_array($id, old('tags', [])) || $mlog->tags->contains($id)) ? 'selected' : '' }}>{{ $tag }}</option>
-                                @endforeach
-                            </select>
-                            @if($errors->has('tags'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('tags') }}
-                                </div>
-                            @endif
-                            <span class="help-block">{{ trans('cruds.mlog.fields.tags_helper') }}</span>
+                            <span class="help-block">{{ trans('cruds.mLog.fields.code_helper') }}</span>
                         </div>
                         <div class="form-group">
                             <button class="btn btn-danger" type="submit">
