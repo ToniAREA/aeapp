08969db90c8fcd7806854389bfa97515f90128d1
ToniAREA <43147720L@gmail.com>
2023-08-21 21:55:35 +0200

    Working on seeders

--- database/seeders/BoatsSeeder.php
+++ database/seeders/BoatsSeeder.php
@@ -22,38 +22,46 @@ class BoatsSeeder extends Seeder
         $last_id++;
 
         // Insertar los datos en la tabla destino de la nueva base de datos
-        for($i = 1; $i < $last_id; $i++){
-
-            //check next id in boats table
-            $this->command->line("Next id in boats table: {$i}");
+        for ($i = 1; $i < $last_id; $i++) {
 
             //find boat with $i in $oldBoats array
             $boat = $oldBoats->where('id', $i)->first();
             // if $boat is null, make a dummy boat with id $i and name 'empty boat'
-            if($boat == null){
+            if ($boat == null) {
                 $boat = (object) [
-                    'name' => 'empty boat',
+                    'boat_type' => '',
+                    'name' => '------',
+                    'imo' => '',
+                    'mmsi' => '',
                     'notes' => '',
                     'internalnotes' => '',
                     'coordinates' => '',
+                    'link' => '',
                 ];
                 DB::table('boats')->insert([
+                    'boat_type' => $boat->boat_type,
                     'name' => $boat->name,
+                    'imo' => $boat->imo,
+                    'mmsi' => $boat->mmsi,
                     'notes' => $boat->notes,
                     'internalnotes' => $boat->internalnotes,
                     'coordinates' => $boat->coordinates,
+                    'link' => $boat->link,
                 ]);
-            }else{
-                //check that $i is the same as $boat->id
-                if($i != $boat->id){
+            } else {
+                if ($i != $boat->id) {
                     $this->command->line("Error: {$i} is not the same as {$boat->id}");
-                }else{
+                } else {
                     $this->command->line("{$i} is the same as {$boat->id}");
                     DB::table('boats')->insert([
+                        'boat_type' => $boat->boat_type,
                         'name' => $boat->name,
+                        'imo' => $boat->imo,
+                        'mmsi' => $boat->mmsi,
                         'notes' => $boat->notes,
                         'internalnotes' => $boat->internalnotes,
                         'coordinates' => $boat->coordinates,
+                        'link' => $boat->link,
                     ]);
                 }
             }
--- database/seeders/ClientsSeeder.php
+++ database/seeders/ClientsSeeder.php
@@ -24,15 +24,12 @@ class ClientsSeeder extends Seeder
         // Insertar los datos en la tabla destino de la nueva base de datos
         for ($i = 1; $i < $last_id; $i++) {
 
-            //check next id in clients table
-            $this->command->line("Next id in clients table: {$i}");
-
             //find client with $i in $oldClients array
             $client = $oldClients->where('id', $i)->first();
             // if $client is null, make a dummy client with id $i and name 'empty client' 
             if ($client == null) {
                 $client = (object) [
-                    'name' => 'empty client',
+                    'name' => '------',
                     'lastname' => '',
                     'vat' => '',
                     'address' => '',
@@ -45,19 +42,6 @@ class ClientsSeeder extends Seeder
                     'link_fd' => '',
                     'coordinates' => '',
                 ];
-            }
-
-            $this->command->line("Client with id {$i} in oldClients array: {$client->name}");
-
-            //check if client exists in clients table
-            $client_exists = DB::connection('mysql')->table('clients')
-                ->where('id', $i)
-                ->exists();
-
-            if ($client_exists) {
-                $this->command->line("Client with id {$i} exists in clients table");
-            } else {
-                $this->command->line("Client with id {$i} does not exist in clients table");
                 DB::table('clients')->insert([
                     'name' => $client->name,
                     'lastname' => $client->lastname,
@@ -72,7 +56,27 @@ class ClientsSeeder extends Seeder
                     'link' => $client->link_fd,
                     'coordinates' => $client->coordinates,
                 ]);
-                $this->command->line("Client with id {$i} inserted in clients table");
+            } else {
+                //check that $i is the same as $client->id
+                if ($i != $client->id) {
+                    $this->command->line("Error: {$i} is not the same as {$client->id}");
+                } else {
+                    $this->command->line("{$i} is the same as {$client->id}");
+                    DB::table('clients')->insert([
+                        'name' => $client->name,
+                        'lastname' => $client->lastname,
+                        'vat' => $client->vat,
+                        'address' => $client->address,
+                        'country' => $client->country,
+                        'telephone' => $client->phone,
+                        'mobile' => $client->mobile,
+                        'email' => $client->email,
+                        'notes' => $client->notes,
+                        'internalnotes' => $client->internalnotes,
+                        'link' => $client->link_fd,
+                        'coordinates' => $client->coordinates,
+                    ]);
+                }
             }
         }
     }
